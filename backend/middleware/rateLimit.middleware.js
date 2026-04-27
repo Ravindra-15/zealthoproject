03a5d80d3144001b01f2a937d54b8d3fb4d5430c
Ravindra Kumar <ravindrakumar15075@gmail.com>
2026-04-27 19:09:36 +0530

    Rate limit Modified

--- backend/middleware/rateLimit.middleware.js
+++ backend/middleware/rateLimit.middleware.js
@@ -4,7 +4,7 @@ const rateLimit = require("express-rate-limit");
 
 exports.otpLimiter = rateLimit({
   windowMs: 60 * 1000, // 1 minute
-  max: 3, // max 3 requests per minute
+  max: 5, // max 5 requests per minute
   message: {
     success: false,
     message: "Too many OTP requests. Please try again later.",
