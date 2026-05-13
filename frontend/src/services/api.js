8b362ff8062ae6bd951c0baa83f31d454fe95014
Ravindra Kumar <ravindrakumar15075@gmail.com>
2026-05-13 18:44:26 +0530

    Base Url Changed in Env

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const API = axios.create({
   // baseURL: import.meta.env.VITE_API_BASE_URL, 
-  baseURL:"https://zealtho.delicode.com" , // e.g. http://localhost:5000/api
+  // baseURL:"https://zealtho.delicode.com" , 
+  baseURL: import.meta.env.VITE_API_BASE_URL,// e.g. http://localhost:5000/api
   withCredentials: true, // 🔥 important for cookies (future use)
 });
 
